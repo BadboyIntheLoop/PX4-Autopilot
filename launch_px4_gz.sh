1129368d3bb48470e52f61965d4054f3fc417c2d
BadboyIntheLoop <chungnt28.uav@outlook.com>
2025-12-16 15:54:43 +0700

    Moderate noise sensor and fix for appropriate with .world

---
+++ launch_px4_gz.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Source Gazebo environment
+source build/px4_sitl_default/rootfs/gz_env.sh
+
+# Set airframe (optional, can be overridden)
+# export PX4_SYS_AUTOSTART=${PX4_SYS_AUTOSTART:-4001}
+
+# Run PX4
+./build/px4_sitl_default/bin/px4
